014a0a0ef1e1fbe10c9131e0a4d7ae5c6c9aa561
autumn-ragland <raglanda998@gmail.com>
2020-09-01 16:08:29 -0500

    add comments

--- Classwork/Controllers/StudentsController.cs
+++ Classwork/Controllers/StudentsController.cs
@@ -23,16 +23,23 @@ namespace Practice.Controllers
         // get method to view all items in db
         public IActionResult ViewAll()
         {
+            // pass object to default view
             return View(_context);
         }
         // get method to one item in db
         public IActionResult ViewOne(int id)
         {
+            // return object from student bd set of StudentsDbContext with the ID matching the ID param
             StudentModel matchingStudent = _context.students.FirstOrDefault(Student => Student.id == id);
+            // if first or default returns a student object
             if(matchingStudent != null)
             {
+                // pass student object to default view
                 return View(matchingStudent);
-            } else {
+            } else 
+            // otherwise
+            {
+                // pass view data to specificed view
                 ViewData["studentID"] = id;
                 return View("NotFound");
             }
@@ -41,37 +48,57 @@ namespace Practice.Controllers
         [HttpPost]
         public IActionResult AddStudent(string name, int classworkAvg)
         {
+            // create new student object from params
             StudentModel newStudent = new StudentModel(){name = name, classworkAvg = classworkAvg};
+            // add new student to StudentDbContext DbSet
             _context.Add(newStudent);
+            // save changes to database (add record)
             _context.SaveChanges();
+            // pass db context to specificed view
             return View("ViewAll", _context);
         }
         // put method to update value of exiting db item
         [HttpPut]
         public IActionResult UpdateClassworkAvg(int id, int classworkAvg)
         {
+            // return object from student bd set of StudentsDbContext with the ID matching the ID param
             StudentModel matchingStudent = _context.students.FirstOrDefault(Student => Student.id == id);
+            // if first or default returns a student object
             if(matchingStudent != null)
             {
+                // update classwork average property to param
                 matchingStudent.classworkAvg = classworkAvg;
+                // save change to data base (update record)
                 _context.SaveChanges();
+                // pass db context to specificed view
                 return View("ViewAll", _context);
-            } else {
+            } else 
+            // otherwise
+            {
+                // pass view data to specificed view
                 ViewData["studentID"] = id;
                 return View("NotFound");
             }
         }
         // delete method to delete db item
         [HttpDelete]
-        public IActionResult DeleteStudent(int id, int hoursPlayed)
+        public IActionResult DeleteStudent(int id)
         {
+            // return object from student bd set of StudentsDbContext with the ID matching the ID param
             StudentModel matchingStudent = _context.students.FirstOrDefault(Student => Student.id == id);
+            // if first or default returns a student object
             if(matchingStudent != null)
             {
+                // remove student object from database
                 _context.Remove(matchingStudent);
+                // save changes to database (remove record)
                 _context.SaveChanges();
+                // pass db context to specificed view
                 return View("ViewAll", _context);
-            } else {
+            } else 
+            // otherwise
+            {
+                // pass view data to specificed view
                 ViewData["StudentID"] = id;
                 return View("NotFound");
             }
